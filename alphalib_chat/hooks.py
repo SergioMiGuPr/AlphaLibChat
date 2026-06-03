c4f3cc57d92009086d316f8cba94bfeb8a622cda
Sergio <sergio.guedess@hotmail.com>
2026-06-03 14:10:00 +0200

    feat: Adding email notification

--- alphalib_chat/hooks.py
+++ alphalib_chat/hooks.py
@@ -5,3 +5,9 @@ app_description = "Chat IA + Conseiller pour ERPNext"
 app_email = "contact@alphalib.fr"
 app_license = "MIT"
 required_apps = ["frappe"]
+
+doc_events = {
+    "AlphaLib Chat Message": {
+        "after_insert": "alphalib_chat.api.advisor_chat.notify_advisor"
+    }
+}
